3d5879ea9b7765f0eda761e8afe23ce7f8a1ac0f
DanielRogs <danielrocha.dr142@gmail.com>
2024-04-28 12:34:38 -0300

    configurando dotenv

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDatabase = () => {
     console.log('Wait connection to the database');
     mongoose.connect(
-        'mongodb+srv://adminuser:mVqz2n46BBSngB0u@clustermc.qv3bczp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMC'
+        process.env.MONGODB_URL
     )
     .then(() => console.log('MongoDB Atlas Connected'))
     .catch((error) => console.log(error));  
